1ed8f3ec357443d56778036124ecfae042b1a60e
waltershub <waltershub@gmail.com>
2018-01-24 10:20:46 -0500

    input with regular keyboard

--- screens/guest-check-in-out.js
+++ screens/guest-check-in-out.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Constants, Location, Permissions } from 'expo';
 import { Header } from 'react-native-elements';
 
 export default class CheckIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      number: '',
+    };
+  }
+
   render() {
     return (
       <View>
@@ -11,6 +18,11 @@ export default class CheckIn extends React.Component {
           outerContainerStyles={{ height: 300 }}
           backgroundColor="#e9e9e9"
           centerComponent={{ text: 'Guest Check-In', style: { color: '#000', fontSize: 100 } }}
+        />
+        <TextInput
+          keyboardType="number-pad"
+          autoFocus
+          onChangeText={number => this.setState({ number })}
         />
       </View>
     );
